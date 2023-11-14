52fcb0af329d93f8d3d23f6cadff672e5134e88b
jooble-gtm-official <94429871+jooble-gtm-official@users.noreply.github.com>
2023-11-14 16:53:25 +0200

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
 
-import './teleporthq/style.css'
+import './style.css'
+import Home from './views/home'
+import NotFound from './views/not-found'
 
 const App = () => {
   return (
-    <Router/>
+    <Router>
+      <Switch>
+        <Route component={Home} exact path="/" />
+        <Route component={NotFound} path="**" />
+        <Redirect to="**" />
+      </Switch>
+    </Router>
   )
 }
 
